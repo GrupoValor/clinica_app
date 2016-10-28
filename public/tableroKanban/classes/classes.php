759a3e4cee9a8afd035682f26ea78ed05c5e102d
Cristhianrg <Cristhian@Cristhians-MacBook-Pro.local>
2016-10-28 10:22:21 -0500

    26: gitignore update
     gitignore update

--- public/tableroKanban/classes/classes.php
+++ public/tableroKanban/classes/classes.php
@@ -9,7 +9,7 @@ class Display
 
     public function backlog($id) {
         $output = '';
-        $stmt = $this->db->query("SELECT * FROM cobarcom_clinicadb.TA_TAREA WHERE tar_estado = 'backlog' AND cas_id = $id");
+        $stmt = $this->db->query("SELECT * FROM TA_TAREA WHERE tar_estado = 'backlog' AND cas_id = $id");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $output .= '<li class="tarea" id="' . $row['tar_id'] . '">' . $row['tar_nombre'] . '</li>';
         }
@@ -18,7 +18,7 @@ class Display
 
     public function pendiente($id) {
         $output = '';
-        $stmt = $this->db->query("SELECT * FROM cobarcom_clinicadb.TA_TAREA WHERE tar_estado = 'pendiente' AND cas_id = $id");
+        $stmt = $this->db->query("SELECT * FROM TA_TAREA WHERE tar_estado = 'pendiente' AND cas_id = $id");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $output .= '<li class="tarea" id="' . $row['tar_id'] . '">' . $row['tar_nombre'] . '</li>';
         }
@@ -27,7 +27,7 @@ class Display
 
     public function proceso($id) {
         $output = '';
-        $stmt = $this->db->query("SELECT * FROM cobarcom_clinicadb.TA_TAREA WHERE tar_estado = 'proceso' AND cas_id = $id");
+        $stmt = $this->db->query("SELECT * FROM TA_TAREA WHERE tar_estado = 'proceso' AND cas_id = $id");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $output .= '<li class="tarea" id="' . $row['tar_id'] . '">' . $row['tar_nombre'] . '</li>';
         }
@@ -36,7 +36,7 @@ class Display
 
     public function finalizada($id) {
         $output = '';
-        $stmt = $this->db->query("SELECT * FROM cobarcom_clinicadb.TA_TAREA WHERE tar_estado = 'finalizada' AND cas_id = $id ");
+        $stmt = $this->db->query("SELECT * FROM TA_TAREA WHERE tar_estado = 'finalizada' AND cas_id = $id ");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $output .= '<li class="tarea" id="' . $row['tar_id'] . '">' . $row['tar_nombre'] . '</li>';
         }
@@ -45,14 +45,14 @@ class Display
 
     public function objetivosCaso($id) {
         $output = '';
-        $stmt = $this->db->query("SELECT cas_objact FROM cobarcom_clinicadb.TA_CASO WHERE cas_id = $id ");
+        $stmt = $this->db->query("SELECT cas_objact FROM TA_CASO WHERE cas_id = $id ");
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         return $row['cas_objact'];
     }
 
     public function observacionesCaso($id) {
         $output = '';
-        $stmt = $this->db->query("SELECT cas_observ FROM cobarcom_clinicadb.TA_CASO WHERE cas_id = $id ");
+        $stmt = $this->db->query("SELECT cas_observ FROM TA_CASO WHERE cas_id = $id ");
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         return $row['cas_observ'];
     }
@@ -60,7 +60,7 @@ class Display
 
     /*public function comentarios($tarea_id) {
         $output = '';
-        $stmt = $this->db->query("SELECT * FROM cobarcom_clinicadb.TA_COMENTARIO WHERE tar_id = $tarea_id");
+        $stmt = $this->db->query("SELECT * FROM TA_COMENTARIO WHERE tar_id = $tarea_id");
         while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
             $output .= '<li class="comentario" id="' . $row['com_id'] . '">' . $row['com_contenid'] . '</li>';
         }
