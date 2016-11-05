c93dc0df2d33ebfb4ffcd94bb4b56bdcd32b0dbf
nataliapalomares <npalomares@pucp.pe>
2016-11-04 19:26:58 -0500

    .

--- public/tableroKanban/php-includes/connect.inc.php
+++ public/tableroKanban/php-includes/connect.inc.php
@@ -1,9 +1,9 @@
 <?php
 try {
-//$db = new PDO( 'mysql:host=murano.websitewelcome.com;dbname=cobarcom_clinicadb;charset=utf8', 'cobarcom_root', 'clinica' );
+$db = new PDO( 'mysql:host=murano.websitewelcome.com;dbname=cobarcom_clinicadb;charset=utf8', 'cobarcom_root', 'clinica' );
 
 
-  $db = new PDO( 'mysql:host=localhost;dbname=clinicadb2;charset=utf8', 'root', '' );
+  //$db = new PDO( 'mysql:host=localhost;dbname=clinicadb2;charset=utf8', 'root', '' );
 
 }
 catch(Exception $e) {
