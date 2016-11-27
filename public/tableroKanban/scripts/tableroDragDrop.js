6b1d2f7553baa307286be2c9b138556d373886c3
yegoaguirre <y.egoaguirre@gmail.com>
2016-11-27 13:37:01 -0500

    tablero + miembros

--- public/tableroKanban/scripts/tableroDragDrop.js
+++ public/tableroKanban/scripts/tableroDragDrop.js
@@ -355,4 +355,27 @@ $(document).ready(function () {
       }
     })
 
+    $('#search_text').keyup(function(){
+           var txt = $(this).val();
+           if(txt != '')
+           {
+                $.ajax({
+                     url:"ajax/busca-miembros.ajax.php",
+                     method:"GET",
+                     data:{search:txt},
+                     dataType:"text",
+                     success:function(data)
+                     {
+                          $('#result').html(data);
+                     }
+                });
+           }
+           else
+           {
+                $('#result').html('');
+           }
+      });
+
+
+
 });
