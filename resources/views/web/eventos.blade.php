839793a08cbfd9979a351bc3f29b843629f0de0e
Cristhianrg <Cristhian@Cristhians-MacBook-Pro.local>
2016-11-28 00:46:57 -0500

    Revert "corrigiendo la descripcion de los eventos"
    This reverts commit 7812b86310dc81ac3892bf596ee7ef4599818154.

--- resources/views/web/eventos.blade.php
+++ resources/views/web/eventos.blade.php
@@ -111,7 +111,7 @@
 														<label class="col-sm-3 control-label no-padding-right" for="form-field-3"> Descripción </label>
 
 														<div class="col-sm-9">
-															<textarea id="descripcion" type="text" id="form-field-3"  class="col-xs-5 col-sm-7" disabled/ ></textarea>
+															<input id="descripcion" type="text" id="form-field-3"  class="col-xs-5 col-sm-7" disabled/ />
 									   	
 														</div>
 													</div>
