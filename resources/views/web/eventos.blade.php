d8f1982ca32b8c5964c9c764d0d239c7586f14d4
AlvaroAT <alvaro.alcarrazt@pucp.pe>
2016-11-27 22:14:20 -0500

    Eventos web completado

--- resources/views/web/eventos.blade.php
+++ resources/views/web/eventos.blade.php
@@ -76,6 +76,89 @@
 							<div class="documentos-modu-wrapper" id="listaEventos">
                             
 							</div>
+							
+							<!-- para ver la descripcion del evento -->
+							<div align="center">        
+								<div class="modal fade" id="boton" role="dialog">
+									<div class="modal-dialog" style="width: 500px;">
+					 
+										<div class="modal-content">
+											<div class="modal-header">
+												<h1 type="button" class="close" data-dismiss="modal"></h1>
+											</div>
+											<!-- Modal content-->
+											<div class="page-header"><!-- /.page-header -->
+												<h1> Detalles del evento</h1>
+
+												<form class="form-horizontal" role="form" style="padding-left: 66px;">
+													<div class="space-20" ></div>
+
+													<div class="form-group">
+														<label class="col-sm-3 control-label no-padding-right" for="form-field-3"> Título </label>
+
+														<div class="col-sm-9">
+															<input id="titulo" type="text" id="form-field-3"  class="col-xs-5 col-sm-7" disabled/ />
+									   	
+														</div>
+													</div>
+                                                               
+                                    
+													<div class="space-4"></div>
+													
+													<div class="space-20" ></div>
+
+													<div class="form-group">
+														<label class="col-sm-3 control-label no-padding-right" for="form-field-3"> Descripción </label>
+
+														<div class="col-sm-9">
+															<input id="descripcion" type="text" id="form-field-3"  class="col-xs-5 col-sm-7" disabled/ />
+									   	
+														</div>
+													</div>
+                                                               
+                                    
+													<div class="space-4"></div>
+                                    
+
+													<div class="form-group">
+														<label class="col-sm-3 control-label no-padding-right" for="form-field-5" > Hora De Inicio </label>
+
+														<div class="col-sm-9">
+															<input id="hor_ini" type="text"   class="col-xs-5 col-sm-7"  disabled/ />
+														</div>
+													</div>    
+                                
+													<div class="space-4"></div>
+                                    
+													<div class="form-group">
+														<label class="col-sm-3 control-label no-padding-right" for="form-field-5" > Hora De Fin </label>
+
+														<div class="col-sm-9">
+															<input id="hor_fin" type="text"   class="col-xs-5 col-sm-7"  disabled/ />
+														</div>
+													</div>
+																
+													<div class="space-4"></div>
+                                    
+													
+													<div class="form-group"></div>				
+									
+													<div class="space-20"></div>								
+												</form>
+
+
+											</div>
+
+											<div class="modal-footer">
+												<div align="center">
+													<button id="boto" type="button" class="btn btn-default" data-dismiss="modal" onclick="close();">Close</button>
+												</div>
+											</div>
+										</div>
+									</div>            
+								</div>
+							</div>
+							
 						</div>
 					</div>
 				</div>
@@ -120,11 +203,30 @@
     <script src="css/wp-content/themes/home-theme/jquery.easing.min.js"></script>
     <script src="css/wp-content/themes/home-theme/jquery.matchHeight-min.js"></script>
     <script src="css/wp-content/themes/home-theme/masonry.pkgd.min.js"></script-->
-    <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
-    <script type="text/javascript" src="http://www.youtube.com/iframe_api"></script>
+    <!-- <script type="text/javascript" src="http://s7.addthis.com/js/250/addthis_widget.js"></script>
+    <script type="text/javascript" src="http://www.youtube.com/iframe_api"></script> -->
+	
     <!--script src="css/wp-content/themes/home-theme/jquery-home.js"></script-->
 	
     <script type="text/javascript">
+		var eventos = [];
+		
+		
+		
+		function add_onClick(id){
+
+			i = parseInt(id);
+
+			$("#descripcion").val(eventos[i][0]);
+			$("#hor_ini").val(eventos[i][1]);
+			$("#hor_fin").val(eventos[i][2]);
+			$("#titulo").val(eventos[i][3]);
+			
+			$("#boton").modal()
+       }
+	
+	
+	
         jQuery(document).ready(function($) {
             
             
@@ -156,16 +258,18 @@
 							
 							if(data[i].visible == 1){
 								
+								var descrip = data[i].description;
 								var titulo = data[i].title;
 								var imagen = data[i].image;
 								var inicio = data[i].start;
 								var fin = data[i].end;
 								
+								eventos.push([descrip,inicio,fin,titulo]);
 								
 								
 								hijo = document.createElement("div");
 				
-								var codigo = '<div class="documentos-modu" id="D"><h2 class="h2-direc"><a href="plantillaEvento.html">'+titulo+'</a></h2><div class="direc-img" data="acf-img"><img src="'+imagen+'"></div><div class="direc-text"><div class="direc-info"><strong>Inicio:</strong>'+' Del '+inicio+' al '+fin+'</div>	<div class="link-btn btn-diplo"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfZKJaZnzker7WHwbJeNJoJDfK4SActHxkgjKxhkMfVcZDYUQ/viewform" target="_blank">Inscribirme<div class="link-btn-icon"></div></a></div></div><div class="clear cero"></div></div>';
+								var codigo = '<div class="documentos-modu" id="D"><h2 class="h2-direc"><a href="javascript:add_onClick('+i+')">'+titulo+'</a></h2><div class="direc-img" data="acf-img"><img src="'+imagen+'"></div><div class="direc-text"><div class="direc-info"><strong>Inicio:</strong>'+' Del '+inicio+' al '+fin+'</div>	<div class="link-btn btn-diplo"><a href="https://docs.google.com/forms/d/e/1FAIpQLSfZKJaZnzker7WHwbJeNJoJDfK4SActHxkgjKxhkMfVcZDYUQ/viewform" target="_blank">Inscribirme<div class="link-btn-icon"></div></a></div></div><div class="clear cero"></div></div>';
 			
 								hijo.innerHTML = codigo;
 								
