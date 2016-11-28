626e72ff095e82e36940efe1ad8a61af5b057594
Cristhianrg <Cristhian@Cristhians-MacBook-Pro.local>
2016-11-28 00:47:04 -0500

    Revert "Merge branch 'master' of https://github.com/GrupoValor/clinica_app"
    This reverts commit e3026e3c98d9bd247f3e85ddeae4ec981b6dd22b, reversing
    changes made to a74d86de59ffcaf32a67ceae985edbcd61096708.

--- public/ajax/listar-alertas.ajax.php
+++ public/ajax/listar-alertas.ajax.php
@@ -1,11 +1,13 @@
 <?php
 header('content-type: application/json; charset=utf-8');
 
-require_once '../php-includes/connect.inc.php';
+require_once '../../php-includes/connect.inc.php';
+
+$caso = $_GET['cas_id'];
 
 $r = array();
 
-$stmt = $db->query("SELECT * FROM TA_ALERTA");
+$stmt = $db->query("SELECT * FROM TA_ALERTA WHERE cas_id = $caso");
 
 while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
     $r[] = $row;
