7f4346f7f3d8c82ac232b525b91fb8cd780a3b24
yegoaguirre <y.egoaguirre@gmail.com>
2016-11-27 19:45:56 -0500

    MAPA MOD

--- public/mapa/ajax/inserta-alerta.ajax.php
+++ public/mapa/ajax/inserta-alerta.ajax.php
@@ -6,7 +6,7 @@ $aler_titulo = $_GET['titulo'];
 $aler_cx = $_GET['cx'];
 $aler_cy = $_GET['cy'];
 
-$stmt = $db->prepare("INSERT INTO ta_alerta(ale_titulo, ale_cx, ale_cy, ale_estado) VALUES(:aler_titulo,:aler_cx,:aler_cy,'registrada')");
+$stmt = $db->prepare("INSERT INTO TA_ALERTA(ale_titulo, ale_cx, ale_cy, ale_estado) VALUES(:aler_titulo,:aler_cx,:aler_cy,'registrada')");
 $stmt->bindParam(':aler_titulo', $aler_titulo);
 $stmt->bindParam(':aler_cx', $aler_cx);
 $stmt->bindParam(':aler_cy', $aler_cy);
