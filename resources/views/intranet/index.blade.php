88485f7823fc4bd2c22709104ab1f91e991eb6c2
Cristhianrg <Cristhian@Cristhians-MacBook-Pro.local>
2016-11-27 21:50:00 -0500

    icons update
    icons update

--- resources/views/intranet/index.blade.php
+++ resources/views/intranet/index.blade.php
@@ -5,7 +5,7 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
 		<meta charset="utf-8" />
 		<title>Intranet</title>
-		<link rel="icon" type="image/png" href="icon/valor.png" />
+		<link rel="icon" type="image/png" href="icono/valor.png" />
 		<meta name="description" content="Draggabble Widget Boxes with Persistent Position and State" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
 
